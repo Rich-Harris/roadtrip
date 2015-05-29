024760eeca8bd744ca82d6de06110bccb9aa53c7
Rich Harris <richard.a.harris@gmail.com>
2015-05-29 14:48:06 -0400

    use rollup instead of esperanto

--- gobblefile.js
+++ gobblefile.js
@@ -1,24 +1,10 @@
 var gobble = require( 'gobble' );
 
-var babelWhitelist = [
-	'es6.arrowFunctions',
-	'es6.blockScoping',
-	'es6.constants',
-	'es6.destructuring',
-	'es6.parameters.default',
-	'es6.parameters.rest',
-	'es6.properties.shorthand',
-	'es6.spread',
-	'es6.templateLiterals'
-];
-
 module.exports = gobble( 'src' )
-	.transform( 'babel', {
-		whitelist: babelWhitelist
-	})
-	.transform( 'esperanto-bundle', {
-		entry: 'roadtrip',
-		type: 'umd',
-		name: 'roadtrip'
-	})
-	.transform( 'sorcery' );
+	.transform( 'babel' )
+	.transform( 'rollup', {
+		entry: 'roadtrip.js',
+		dest: 'roadtrip.js',
+		format: 'umd',
+		moduleName: 'roadtrip'
+	});

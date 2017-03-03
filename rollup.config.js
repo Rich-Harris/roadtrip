5225133ff5da2b524639db02529292d2fb7400b0
Rich Harris <richard.a.harris@gmail.com>
2017-03-03 18:07:03 -0500

    update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
 import buble from 'rollup-plugin-buble';
 
+const pkg = require( './package.json' );
+
 export default {
 	entry: 'src/roadtrip.js',
 	plugins: [ buble() ],
-	moduleName: 'roadtrip'
+	moduleName: 'roadtrip',
+	targets: [
+		{ dest: pkg.main, format: 'umd' },
+		{ dest: pkg.module, format: 'es' }
+	]
 };

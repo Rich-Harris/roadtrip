51004c89c34c5edb4410c75878d6cee435f7b4a3
Rich Harris <richard.a.harris@gmail.com>
2017-11-14 23:21:51 -0500

    add skipped test for svg links

--- test/tests.js
+++ test/tests.js
@@ -420,6 +420,36 @@ describe( 'roadtrip', () => {
 			});
 		});
 	});
+
+	describe( 'svg', () => {
+		it.skip( 'watches svg links', () => { // skip because JSDOM is funny with SVG, apparently
+			return createTestEnvironment().then( window => {
+				const roadtrip = window.roadtrip;
+				const routes = [];
+
+				window.document.body.innerHTML = `
+					<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>
+						<a xlink:href='/foo'>click me</a>
+					</svg>
+				`;
+
+				return roadtrip
+					.add( '/foo', {
+						enter: () => {
+							routes.push( 'foo' );
+						}
+					})
+					.start()
+					.then( () => {
+						const a = window.document.querySelector( 'a' );
+						window.simulant.fire( a, 'click', { which: 1 });
+						assert.deepEqual( routes, [ 'foo' ]);
+
+						window.close();
+					});
+			});
+		});
+	});
 });
 
 function wait ( ms ) {
